ed9a075ebd13d64c88db4cfa2d6369a072477f15
Matt Wittmann <mcwitt@gmail.com>
2021-03-13 12:04:59 -0800

    Upgrade hls, remove custom derivation
    No longer needed since recent versions are on Hackage

--- shell.nix
+++ shell.nix
@@ -21,7 +21,7 @@ let
 
     tools = {
       cabal = "3.2.0.0";
-      haskell-language-server = "0.6.0";
+      haskell-language-server = "1.0.0.0";
     };
 
     # Prevents cabal from choosing alternate plans, so that
