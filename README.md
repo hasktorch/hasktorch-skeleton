71b2eadfa6f535e1f69607cbf7abaad271d90a75
Junji Hashimoto <junji.hashimoto@gree.net>
2025-01-06 21:38:03 +0900

    Use hasktorch in nixpkgs

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Similar to the [Cardano Skeleton](https://github.com/input-output-hk/cardano-ske
 this repository serves as an example of how a downstream user of both Nix and Hasktorch
 can set up a development environment.
 
+The Nix shell installs ghc with hasktorch. When there is a hasktorch cache in nixpkgs, building hasktorch will be skipped.
+
 # 3 Steps to happy Hasktorch coding
 
 1. Fork this repo and clone it locally.
-2. Launch a Nix shell with (optionally) CUDA, `ghcide`, and VS Code, `nix-shell --arg cudaSupport true --argstr cudaMajorVersion 10 --command "code ."`
+2. Launch a Nix shell with (optionally) CUDA, `hls`, and VS Code, `nix develop"`
 3. Install the [Haskell Language Server plugin](https://marketplace.visualstudio.com/items?itemName=alanz.vscode-hie-server) and set the `HIE Variant` to `ghcide`.
 
 Happy Hasktorch hacking!
--- flake.nix
+++ flake.nix
@@ -1,112 +1,35 @@
 {
+  nixConfig = {
+    bash-prompt = "\[hasktorch-skeleton$(__git_ps1 \" (%s)\")\]$ ";
+  };
   inputs = {
-    haskell-nix.url = "github:input-output-hk/haskell.nix";
-    stackageSrc = {
-      url = "github:input-output-hk/stackage.nix";
-      flake = false;
-    };
-    hackageSrc = {
-      url = "github:input-output-hk/hackage.nix";
-      flake = false;
-    };
     utils.url = "github:numtide/flake-utils";
-    libtorch-nix = {
-      url = "github:hasktorch/libtorch-nix/d14b0fd10b96d192b92b8ccc7254ade4b3489331";
-      flake = false;
-    };
+    nixpkgs.url = "github:junjihashimoto/nixpkgs?rev=5c77923028d8ec6c54dc9820e44bced372c68f3d";
   };
 
-  outputs = inputs@{ self, nixpkgs, haskell-nix, utils, ... }:
-    let
-      name = "hasktorchSkeleton";
-      compiler = "ghc8104"; # Not used for `stack.yaml` based projects.
-      cudaSupport = false;
-      cudaMajorVersion = null;
-      project-name = "${name}HaskellPackages";
-
-
-      # This overlay adds our project to pkgs
-      project-overlay = final: prev: {
-        ${project-name} =
-            #assert compiler == supported-compilers;
-            final.haskell-nix.project' {
-              # 'cleanGit' cleans a source directory based on the files known by git
-              src = prev.haskell-nix.haskellLib.cleanGit {
-                inherit name;
-                src = ./.;
-              };
-
-              compiler-nix-name = compiler;
-              projectFileName = "cabal.project"; # Not used for `stack.yaml` based projects.
-              modules = [
-                # Fixes for libtorch-ffi
-                {
-                  packages.libtorch-ffi = {
-                    configureFlags = with final; [
-                      "--extra-lib-dirs=${torch}/lib"
-                      "--extra-include-dirs=${torch}/include"
-                      "--extra-include-dirs=${torch}/include/torch/csrc/api/include"
-                    ];
-                    flags = {
-                      cuda = cudaSupport;
-                      gcc = !cudaSupport && final.stdenv.hostPlatform.isDarwin;
-                    };
-                  };
-                }
-              ];
-
-            };
-
-      };
-    in
-      { overlay = final: prev: {
-          "${name}" = ("${project-name}-overlay" final prev)."${project-name}".flake {};
+  outputs = { self, nixpkgs, utils  }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config.cudaSupport = system == "x86_64-linux";
         };
-      } // (utils.lib.eachSystem [ "x86_64-linux" ] (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              haskell-nix.overlay
-              (final: prev: {
-                haskell-nix = prev.haskell-nix // {
-                  sources = prev.haskell-nix.sources // {
-                    hackage = inputs.hackageSrc;
-                    stackage = inputs.stackageSrc;
-                  };
-                  modules = [
-                    # Fixes for libtorch-ffi
-                    {
-                      packages.libtorch-ffi = {
-                        configureFlags = with final; [
-                          "--extra-lib-dirs=${torch}/lib"
-                          "--extra-include-dirs=${torch}/include"
-                          "--extra-include-dirs=${torch}/include/torch/csrc/api/include"
-                        ];
-                        flags = {
-                          cuda = cudaSupport;
-                          gcc = !cudaSupport && final.stdenv.hostPlatform.isDarwin;
-                        };
-                      };
-                    }
-                  ];
-                };
-              })
-              (import ./nix/overlays/libtorch.nix { inherit inputs cudaSupport cudaMajorVersion; })
-              project-overlay
-            ];
-          };
-          flake = pkgs."${project-name}".flake {};
-        in flake // rec {
-
-          packages.example = flake.packages."${name}:exe:example";
-
-          defaultPackage = packages.example;
-
-          devShell = (import ./shell.nix {
-              inherit cudaSupport cudaMajorVersion pkgs;
-              withHoogle = false;
-            });
-        }
-        ));
+        ghcWithHasktorch = pkgs.haskellPackages.ghcWithPackages (pkgs: with pkgs; [
+          hasktorch
+          haskell-language-server
+        ]);
+      in {
+        defaultPackage = pkgs.haskellPackages.callCabal2nix "hasktorch-skeleton" ./. {};
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            ghcWithHasktorch
+            cabal-install
+            stack
+          ];
+          shellHook = ''
+            source ${git}/share/bash-completion/completions/git-prompt.sh
+          '';
+        };
+      });
 }
